7b9b3571e4461d85639d585f7bda88ccd681f5d7
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-18 01:23:56 +0300

    fixed the framer option

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-project/', // Set this to the subdirectory your site will be served from
-})
+  base: "/my-project/", // Set this to the subdirectory your site will be served from
+  build: {
+    rollupOptions: {
+      external: ["framer-motion"],
+    },
+  },
+});
