75ffdb043f1dc3f19248bd771d607237a48bc2fe
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-18 01:14:23 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-project/', // Set this to the subdirectory your site will be served from
+})
