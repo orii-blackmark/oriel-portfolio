d1f29310a2f14db23d531d669860a91f179010b8
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-20 17:29:07 +0300

    Modified the Maya project files

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/maya.png";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
@@ -40,11 +40,11 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "CPIMS Virtual Assistant-Maya",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Maya is a virtual assistant chatbot built to provide assistance to users of the Child Protection Information Management Service (CPIMS). It is designed to provide users with quick and accurate responses to common queries and questions related to child protection.",
+    technologies: ["Flask", "Tailwind CSS", "HTML", "Js", "Json"],
   },
   {
     title: "Task Management App",
