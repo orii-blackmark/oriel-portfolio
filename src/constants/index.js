dd78519e12468693169766803200fc40be946baa
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-20 16:46:49 +0300

    Modified the contact details

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Nairobi, Kenya",
+  phoneNo: "+254 758 997 669 ",
+  email: "kiplangatoriel@gmail.com",
 };
