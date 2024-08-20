4f79daf2929fcfdbe0e84f2db2aacad12fef748f
orii-blackmark <102928012+orii-blackmark@users.noreply.github.com>
2024-08-20 15:32:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-project/", // Set this to the subdirectory your site will be served from
+  base: './',
   build: {
     rollupOptions: {
       external: [
