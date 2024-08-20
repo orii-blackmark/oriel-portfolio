8f6f2d47af706043bfe66a0b6dcdde0212445193
orii-blackmark <102928012+orii-blackmark@users.noreply.github.com>
2024-08-20 16:30:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        "framer-motion",
-        "react-icons/fa",
-        "react-icons/ri", // Include react-icons/ri
-        "react",
-        "react-dom",
+        // "framer-motion", // Uncomment if you want to exclude this from the build
+        // "react-icons/fa", // Uncomment if you want to exclude this from the build
+        // "react-icons/ri", // Uncomment if you want to exclude this from the build
+        // "react", // Uncomment if you want to exclude this from the build
+        // "react-dom", // Uncomment if you want to exclude this from the build
       ],
     },
   },
